116b95cc579eeeb3069d26e7338d2a1d1508dbb9
Olga Nim <helganim@list.ru>
2024-08-02 17:09:06 +0300

    add solution

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,33 @@ public class Main {
     public static String nameAcc = "MyAcc";
     public static int amountInBankAccount = 1000000;
 
-    public static void main(String[] args) {
-        try {
-            System.out.println(deduct("MyAcc", 2000000));
-        } catch (InsufficientAmountException e) {
-            System.out.println("ошибка");
-        }
+    public static void main(String[] args) throws InsufficientAmountException {
+//        try {
+//            System.out.println(deduct("MyAcc", 2000000));
+//        } catch (InsufficientAmountException e) {
+//            System.out.println("ошибка");
+//        }
 
-        //throws InsufficientAmountException, NameDontMatchException
+        System.out.println(deduct("MyAcc", 10000));
+        System.out.println(deduct("MyAcc", 2000000));
+        System.out.println(deduct("NotMyAcc", 500));
 
-            System.out.println(deduct("MyAcc", 10000));
-            System.out.println(deduct("MyAcc", 2000000));
-            System.out.println(deduct("NotMyAcc", 500));
-        }
-
+    }
         public static int deduct (String accountToProcess,int amtToCacheOut) throws InsufficientAmountException {
             if (!nameAcc.equals(accountToProcess)) {
-                throw new NameDontMatchException("Название аккаунта не соответствует");
+                System.out.println("Название аккаунта не соответствует");
             }
             if (amountInBankAccount < amtToCacheOut) {
-                throw new InsufficientAmountException("Недостаточно денег на счету, введите меньше " + amtToCacheOut);
+                System.out.println("Недостаточно денег на счету, введите меньше " + amtToCacheOut);
             }
 
             amountInBankAccount = amountInBankAccount - amtToCacheOut;
             return amountInBankAccount;
         }
 
-
-
     }
+
+
 
 
 
